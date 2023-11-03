78c25a62e16f9a6ad2684e7fd3267596250a2e2a
SammytheBelegor <asyrgak@list.ru>
2023-11-03 22:02:24 +0600

    Third Commit

--- src/TextArea.js
+++ src/TextArea.js
@@ -2,32 +2,13 @@ import React, { useState } from 'react';
 
 const TextArea = () => {
   const [text, setText] = useState('');
-<<<<<<< HEAD
-=======
   const [textSize, setTextSize] = useState('16px');
   const [textFont, setTextFont] = useState('Arial');
->>>>>>> 8526c57 (Second Commit)
   const [textColor, setTextColor] = useState('black');
 
   const handleInputChange = (e) => {
     const inputValue = e.target.value;
 
-<<<<<<< HEAD
-    // Add your creative effect or encoding logic here
-    // For example, reversing the input text
-    const reversedText = inputValue.split('').reverse().join('');
-
-    // Add an effect to change the text color
-    const colors = ['red', 'blue', 'green', 'orange', 'purple'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-
-    setText(reversedText);
-    setTextColor(randomColor);
-  };
-
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-=======
     // You can customize the text size, font, and color based on user input
     // For example, change text size, font, and color dynamically
     // You can add your logic to change these properties as needed
@@ -51,21 +32,16 @@ const TextArea = () => {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
->>>>>>> 8526c57 (Second Commit)
       <textarea
         rows="4"
         cols="50"
         placeholder="Type something..."
         onChange={handleInputChange}
-<<<<<<< HEAD
-        style={{ color: textColor }}
-=======
         style={{
           color: textColor,
           fontSize: textSize,
           fontFamily: textFont,
         }}
->>>>>>> 8526c57 (Second Commit)
         value={text}
       />
     </div>
